be71917fcb0015839b694ab487418f19f1c5318e
yana-shkvarovskaya <shkvarovskayayana@gmail.com>
2021-09-11 19:11:41 +0300

    добавляет анимацию для секции

--- src/js/modal.js
+++ src/js/modal.js
@@ -12,3 +12,31 @@
       refs.modal.classList.toggle('backdrop--hidden');
     }
   })();
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open-two]'),
+      closeModalBtn: document.querySelector('[data-modal-close-two]'),
+      modal: document.querySelector('[data-modal-two]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('backdrop--hidden');
+    }
+  })();
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open-three]'),
+      closeModalBtn: document.querySelector('[data-modal-close-three]'),
+      modal: document.querySelector('[data-modal-three]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('backdrop--hidden');
+    }
+  })();
