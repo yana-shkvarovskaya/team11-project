0092c5805ceaa13cbf3cd41699539b789477ec55
yana-shkvarovskaya <shkvarovskayayana@gmail.com>
2021-09-13 12:39:44 +0300

    section about

--- src/js/modal.js
+++ src/js/modal.js
@@ -40,3 +40,17 @@
       refs.modal.classList.toggle('backdrop--hidden');
     }
   })();
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-about-modal-open]'),
+      closeModalBtn: document.querySelector('[data-about-modal-close]'),
+      modal: document.querySelector('[data-about-modal]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('backdrop--hidden');
+    }
+  })();
