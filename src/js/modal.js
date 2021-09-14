accf595120277927d2695ca0993ce456a90bd1dd
Ann456 <popovaa769@gmail.com>
2021-09-14 20:51:47 +0300

    модальное окно

--- src/js/modal.js
+++ src/js/modal.js
@@ -54,3 +54,18 @@
       refs.modal.classList.toggle('backdrop--hidden');
     }
   })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open-header]'),
+      closeModalBtn: document.querySelector('[data-modal-close-header]'),
+      modal: document.querySelector('[data-modal-header]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('backdrop--hidden');
+    }
+  })();
