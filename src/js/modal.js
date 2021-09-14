c88d2c25372e956081a6e87109e68e3f40be7229
Ann456 <popovaa769@gmail.com>
2021-09-15 00:37:44 +0300

    добавлено модальное окно для мобильного меню

--- src/js/modal.js
+++ src/js/modal.js
@@ -69,3 +69,18 @@
       refs.modal.classList.toggle('backdrop--hidden');
     }
   })();
+
+    (() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open-header-mobile]'),
+      closeModalBtn: document.querySelector('[data-modal-close-header-mobile]'),
+      modal: document.querySelector('[data-modal-header-mobile]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('backdrop--hidden');
+    }
+  })();
