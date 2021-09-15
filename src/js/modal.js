529f0906b1764b412ebdea070674f27770570400
Hanna88-A <annamukolaivna88@gmail.com>
2021-09-15 08:16:19 +0300

    Вносит исправления

--- src/js/modal.js
+++ src/js/modal.js
@@ -68,3 +68,18 @@
     refs.modal.classList.toggle('backdrop--hidden');
   }
 })();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-locations]'),
+    closeModalBtn: document.querySelector('[data-modal-close-locations]'),
+    modal: document.querySelector('[data-modal-locations]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('backdrop--hidden');
+  }
+})();
+
