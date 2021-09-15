eef94c66df079b5dc4388c639f7a9f7546b337bf
yana-shkvarovskaya <shkvarovskayayana@gmail.com>
2021-09-15 10:09:39 +0300

    changes

--- src/js/modal.js
+++ src/js/modal.js
@@ -68,18 +68,18 @@
     refs.modal.classList.toggle('backdrop--hidden');
   }
 })();
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open-locations]'),
-    closeModalBtn: document.querySelector('[data-modal-close-locations]'),
-    modal: document.querySelector('[data-modal-locations]'),
-  };
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open-locations]'),
+//     closeModalBtn: document.querySelector('[data-modal-close-locations]'),
+//     modal: document.querySelector('[data-modal-locations]'),
+//   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('backdrop--hidden');
-  }
-})();
+//   function toggleModal() {
+//     refs.modal.classList.toggle('backdrop--hidden');
+//   }
+// })();
 
